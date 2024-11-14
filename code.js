600c0ebf0cd9f2ce371bc4f0ebcf0dd158636bfb
jataylor111 <143449478+jataylor111@users.noreply.github.com>
2024-11-14 15:06:28 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,10 @@
-function tailRecFib(n) {}
+function tailRecFib(n) { return doFib(n, 1, 0); }
+
+function doFib(n, b, acc) {
+    if(n == 0) return acc;
+    else return doFib(n - 1, b + acc, b); 
+}
+
+
+// n = 15;
+// console.log(tailRecFib(n));
