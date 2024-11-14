da11fd419dbc172f53723ece80c7347bee6768cd
jataylor111 <143449478+jataylor111@users.noreply.github.com>
2024-11-14 15:31:04 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,9 @@
 function tailRecFib(n) { return doFib(n, 1, 0); }
 
 function doFib(n, b, acc) {
+    // Once n is zero return the result
     if(n == 0) return acc;
+    // I had the right idea but I was sending the wrong parts to the wrong declarations
     else return doFib(n - 1, b + acc, b); 
 }
 
